51a6ba9ac55c22d695fb6594166fd5cc141977ea
Imtiaz Epu <imtiazepu@programmer.net>
2019-07-18 01:29:39 +0600

    server-side integration

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,6 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use PayPal\Api\Amount;
+use PayPal\Api\Details;
+use PayPal\Api\Payment;
+use PayPal\Api\PaymentExecution;
+use PayPal\Api\Transaction;
 
 class PaymentController extends Controller
 {
@@ -14,7 +19,28 @@ class PaymentController extends Controller
                 'EN5IlD9QF7tu7ZHLeC3RHrOTDq7uM2SCY9DA_96wl4pFU8-ttyC-WJe1xi_0w-ZLEG2oLivc0IQvXSSz'
             )
         );
+        $paymentId = request('paymentId');
+        $payment = Payment::get($paymentId, $apiContext);
 
-        return \request('paymentId');
+        $execution = new PaymentExecution();
+        $execution->setPayerId(\request('PayerID'));
+
+        $transaction = new Transaction();
+        $amount = new Amount();
+        $details = new Details();
+
+        $details->setShipping(2.2)
+            ->setTax(1.3)
+            ->setSubtotal(17.50);
+
+        $amount->setCurrency('USD');
+        $amount->setTotal(21);
+        $amount->setDetails($details);
+        $transaction->setAmount($amount);
+
+        $execution->addTransaction($transaction);
+        $result = $payment->execute($execution, $apiContext);
+
+        return $result;
     }
 }
